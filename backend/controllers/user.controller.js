3f1d2ff235240d43c7d93049518deadb534db4ef
Subrat <subratgupta2704@gmail.com>
2025-04-12 09:01:18 +0530

    User Controller setup

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,159 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {
+  try {
+    const { fullName, email, phoneNumber, password, role } = req.body;
+    if (!fullName || !email || !phoneNumber || !password || !role)
+      return res.status(400).json({
+        message: "Something is missing",
+        success: false,
+      });
+
+    const user = await User.findOne({ email });
+    if (user)
+      return res.status(400).json({
+        message: "User already exists with this email ID",
+        success: false,
+      });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      fullName,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+      role,
+    });
+
+    return res.status(201).json({
+      message: "User created successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role)
+      return res.status(400).json({
+        message: "Something is missing",
+        success: false,
+      });
+
+    let user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({
+        message: "User not found",
+        success: false,
+      });
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch)
+      return res.status(400).json({
+        message: "Invalid credentials",
+        success: false,
+      });
+
+    if (role != user.role)
+      return res.status(400).json({
+        message: "Account doesn't exist with this role",
+        success: false,
+      });
+
+    const tokenData = {
+      userId: user._id,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    user = {
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpsOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: `Welcome back ${user.fullName}`,
+        user,
+        success: true,
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      message: "Logged out successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { fullName, email, phoneNumber, bio, skills } = req.body;
+    if (!fullName || !email || !phoneNumber || !bio || !skills) {
+      return res.status(400).json({
+        message: "Something is missing",
+        success: false,
+      });
+    }
+
+    const skillsArray = skills.split(",");
+    const userId = req.id;
+    let user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found.",
+        success: false,
+      });
+    }
+
+    //Updating the data
+    (user.fullName = fullName),
+      (user.email = email),
+      (user.phoneNumber = phoneNumber),
+      (user.profile.bio = bio),
+      (user.profile.skills = skillsArray);
+
+    await user.save();
+
+    user = {
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+
+    return res.status(200).json({
+      message: "Profile updated successfully.",
+      user,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
