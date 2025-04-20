e6630fa9fb0bc6fb76be7680e4726ee75fd85dfc
Subrat <subratgupta2704@gmail.com>
2025-04-20 13:56:41 +0530

    Home Page - Hero Section Created

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Input } from "./ui/Input";
+import { Button } from "./ui/Button";
+import { Search } from "lucide-react";
+
+const HeroSection = () => {
+  return (
+    <div className="text-center">
+      <div className="flex flex-col gap-5 my-10">
+        <span className="mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium">
+          No.1 Job Hunt Website
+        </span>
+        <h1 className="text-5xl font-bold">
+          Search, Apply & <br />
+          Get your<span className="text-[#6A38C2]">Dream Jobs</span>
+        </h1>
+        <p>
+          Whether you're just starting out or looking for your next big move —
+          your dream job is just a few clicks away.
+        </p>{" "}
+        <div className="flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto">
+          <Input
+            className="outline-none border-none w-full"
+            type="text"
+            placeholder="Find your dream jobs"
+          />
+          <Button className="rounded-r-full bg-[#6A38C2]">
+            <Search className="h-5 w-5" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import Navbar from "../components/shared/Navbar";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HeroSection />
+      {/* <CategoryCarousel />
+      <LatestJobs />
+      <Footer /> */}
     </div>
   );
 };
