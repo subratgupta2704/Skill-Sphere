9269b8ab1ac24cb77832dceec8205566caf5b404
Subrat <subratgupta2704@gmail.com>
2025-05-07 20:35:19 +0530

    Added Framer Motion

--- frontend/src/screens/Jobs.jsx
+++ frontend/src/screens/Jobs.jsx
@@ -5,6 +5,7 @@ import Job from "@/components/Job";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 const Jobs = () => {
   const { allJobs, searchedQuery } = useSelector((state) => state.job);
@@ -15,7 +16,9 @@ const Jobs = () => {
       const filteredJobs = allJobs.filter((job) => {
         return (
           job.title.toLowerCase().includes(searchedQuery.toLowerCase()) ||
-          job.company.name.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+          job.company.name
+            .toLowerCase()
+            .includes(searchedQuery.toLowerCase()) ||
           job.location.toLowerCase().includes(searchedQuery.toLowerCase()) ||
           job.description.toLowerCase().includes(searchedQuery.toLowerCase())
         );
@@ -42,7 +45,15 @@ const Jobs = () => {
             <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
               <div className="grid grid-cols-3 gap-4">
                 {filterJobs.map((job) => (
-                  <Job key={job?._id} job={job} />
+                  <motion.div
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 0.3 }}
+                    key={job?._id}
+                  >
+                    <Job job={job} />
+                  </motion.div>
                 ))}
               </div>
             </div>
